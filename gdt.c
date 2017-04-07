8c463f6c104afaac44926349d109d6c134664479
Tristan Dube <tristan.dube@gmail.com>
2017-04-06 21:18:07 -0400

    GDT code cleanup and expansion
     - Added descriptor 3 and 4 for user mode code and data segments.
    
     - Code documentation of the GDT flags and structure. Some fields
    reference the Intel Reference books,

--- gdt.c
+++ gdt.c
@@ -1,8 +1,41 @@
 #include "gdt.h"
 
-gdtEntry_t gdt[3];
+gdtEntry_t gdt[5];
 
-void gdtSetGate(int num, uint64_t base, uint64_t limit, uint8_t access, uint8_t gran){
+/*
+ * Access byte
+ * Bit :    | 7 | 6 5 | 4  | 3 2 1 0 |
+ * Content: | P | DPL | DT |  TYPE   |
+ *
+ * P = Segment present bit, 1 for yes
+ * DPL = Descriptor privilege level, Ring 0 to 3
+ * DT = Descriptor type. 0 for system, 1 for code or data.
+ * TYPE = Code and data segment type. See Intel Vol. 3A 3-12
+ *
+ * Usual values :
+ * 0x9A = 1 0 0 1           Kernel mode
+ *                 1 0 1 0  Code segment Execute/Read
+ * 0x92 = 1 0 0 1           Kernel mode
+ *                 0 0 1 0  Data segment Read/Write
+ * 0xFA = 1 1 1 1           User mode
+ *                 1 0 1 0  Code segment Execute/Read
+ * 0xF2 = 1 1 1 1           User mode
+ *                 0 0 1 0  Data segment Read/Write
+ *
+ * Granularity byte
+ * Bit :    | 7 | 6 | 5 | 4 | 3 2 1 0 |
+ * Content: | G | D | 0 | A |    0    |
+ *
+ * G = Granularity. 0 = 1 byte, 1 = 1kb
+ * D = Operand Size, 0 = 16bit, 1 = 32bit
+ * A = Available.
+ *
+ * Usual values :
+ * 0xCF = 1 1 0 0           4KB pages
+ *                 1 1 1 1  Not used
+ */
+void gdtSetGate(int num, uint64_t base, uint64_t limit, uint8_t access, uint8_t gran)
+{
    gdt[num].lBase      = (base & 0xFFFF);
    gdt[num].mBase      = (base >> 16) & 0xFF;
    gdt[num].hBase      = (base >> 24) & 0xFF;
@@ -14,13 +47,16 @@ void gdtSetGate(int num, uint64_t base, uint64_t limit, uint8_t access, uint8_t 
    gdt[num].access      = access;
 }
 
-void setupGdt(){
+void setupGdt()
+{
    gdtPointer.limit   = (sizeof(gdtEntry_t) * 3) - 1;
    gdtPointer.base      = (uint32_t)&gdt;
 
    gdtSetGate(0, 0, 0, 0, 0);
    gdtSetGate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF);
    gdtSetGate(2, 0, 0xFFFFFFFF, 0x92, 0xCF);
+   gdtSetGate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF);
+   gdtSetGate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF);
 
    loadGdt();
 }
--- gdt.h
+++ gdt.h
@@ -1,5 +1,25 @@
+#ifndef GDT_H
+#define GDT_H
+
 #include <stdint.h>
 
+/*
+ * Segment Descriptor
+ * Intel Vol. 3A 3-10
+ *
+ * HIGH DWORD
+ * | 31 .. 24 | 23 | 22 | 21 | 20 | 19 .. 16
+ * |   hBase  | G  |DIB | L  |AVL |  hLimit
+ *
+ * | 15 | 14 13 | 12 | 11 .. 8 | 7 .. 0
+ * | P  |  DPL  | S  |   TYPE  |  mBase
+ *
+ * LOW DWORD
+ * | 31 .. 16 |
+ * |  lBase   |
+ * | 15 .. 0  |
+ * |  lLimit  |
+ */
 typedef struct {
    uint16_t           lLimit;
    uint16_t           lBase;
@@ -17,3 +37,8 @@ struct {
 extern void loadGdt();
 
 void setupGdt();
+
+
+
+
+#endif
