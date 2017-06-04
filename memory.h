ed8380691904a4ab0cd77feae9e080c63e6f1011
Tristan Dube <tristan.dube@gmail.com>
2017-06-03 21:47:35 -0400

    Cleanup in the Paging functions.

--- memory.h
+++ memory.h
@@ -71,11 +71,9 @@ alloc_unit_t largePool[large_pool_size];
 
 // Paging Stuff
 #define PAGE_ALL_PRESENT
-//uint32_t page_directory[1024] __attribute__((aligned(4096)));
+
 uint32_t page_directory[1024] __attribute__((aligned(4096)));
 uint32_t page_tables[1024*1024] __attribute__((aligned(4096)));
-
-uint32_t first_page_table[1024] __attribute__((aligned(4096)));
 
 void setup_paging();
 
