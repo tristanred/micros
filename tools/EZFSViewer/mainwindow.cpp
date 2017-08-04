8efe799499f26bda3f85d53bcc0694d2104418cb
Tristan Dubé <tristan.dube@gmail.com>
2017-08-04 08:22:32 -0400

    EZFS Viewer upgrade
    Added more options and started on the disk load sequence.

--- tools/EZFSViewer/mainwindow.cpp
+++ tools/EZFSViewer/mainwindow.cpp
@@ -1,14 +1,30 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFileDialog>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->tabWidget->setEnabled(false);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_btnLoadDisk_clicked()
+{
+    QString diskFile = QFileDialog::getOpenFileName(this, "Select an ezfs disk file.");
+
+    if(diskFile != "")
+    {
+        ui->txtDiskFilePath->setText(diskFile);
+
+        ui->tabWidget->setEnabled(true);
+    }
 }
