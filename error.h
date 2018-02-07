d030b8a7d87950c736491f056153ccdea107c31b
Tristan Dube <tristan.dube@gmail.com>
2018-02-06 19:51:27 -0500

    Error codes
    Started to add error codes. Integer defines assign error codes to error
    names.

--- error.h
+++ error.h
@@ -2,9 +2,13 @@
 #define ERROR_H
 
 #include "types.h"
+#include "errorcodes.h"
 
+// This value is set to true when the kernel panic'd and bailed out of 
+// execution.
 BOOL panic;
 
+// If c is NOT true, panic.
 #define ASSERT(c, m) if((c) == FALSE) PanicQuit(m)
 
 // Dummy function that gdb breaks into
@@ -14,7 +18,6 @@ void PanicQuit(char* message);
 
 void ShowErrorMessage(char* errorType, char* message);
 
-//
 void TemplateFault(char* msg);
 
 void DivideByZeroFault();
---
+++ errorcodes.h
@@ -0,0 +1,13 @@
+#ifndef ERRORCODES_H
+#define ERRORCODES_H
+
+/**
+ * System error code list. Trying to follow the error names from Linux as 
+ * much as possible. The error codes are not the same but if a name is found in
+ * Linux it should have a simmilar meaning.
+ */
+
+#define ENOMEM 1 // Out of memory
+#define EINVAL 2 // Invalid function argument
+
+#endif
