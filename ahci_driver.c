4138e043be6f1ae573f3672cffcbdf9fb6cce593
Tristan Dubé <tristan.dube@gmail.com>
2018-07-27 22:42:34 -0400

    AHCI work.
    Fixed the defines to get the flags, the (x) parameter catch need to be
    adjacent to the define symbol without spaces.
    
    Started adding some functions impl.

--- ahci_driver.c
+++ ahci_driver.c
@@ -1,1 +1,102 @@
 #include "ahci_driver.h"
+
+#include "pci.h"
+#include "memory.h"
+#include "flagutils.h"
+
+uint32_t driver_ahci_get_default_disk()
+{
+    int total = 0;
+    struct pci_controlset* set = get_devices_list(&total);
+    
+    struct pci_device* dev = NULL;
+    for(int i = 0; i < total; i++)
+    {
+        if(set->deviceList[i]->classCode == PCI_CLA_MASS_STORAGE &&
+           set->deviceList[i]->subClass == PCI_SUB_MASS_STOR_SATA_CONTROLLER )
+           {
+               dev = set->deviceList[i];
+           }
+    }
+
+    if(dev != NULL)
+    {
+        uint32_t abar = dev->barAddress5;
+        
+        return abar;
+    }
+    
+    return 0;
+}
+
+int driver_ahci_read_GHC_regs(uint32_t abar, struct ahci_generic_host_regs* regs)
+{
+    memcpy(regs, (const void*)abar, sizeof(struct ahci_generic_host_regs));
+    
+    return 0;
+}
+
+int driver_ahci_read_port_regs(uint32_t abar, int portNb, struct ahci_port_regs* regs)
+{
+    uint32_t portAddress = abar + 0x100 + (portNb * 0x80);
+    
+    memcpy(regs, (void*)portAddress, sizeof(struct ahci_port_regs));
+    
+    return 0;
+}
+
+int driver_ahci_read_port_commandlist(uint32_t abar, int portNb, struct ahci_port_commandlist* data)
+{
+    uint32_t portAddress = abar + 0x100 + (portNb * 0x80);
+    
+    struct ahci_port_regs* port = (struct ahci_port_regs*)portAddress;
+    
+    uint32_t cmdlistaddr = port->command_list_base_addr_lower;
+    
+    memcpy(data, (struct ahci_port_commandlist*)cmdlistaddr, sizeof(struct ahci_port_commandlist));
+
+    return 0;
+}
+
+int driver_ahci_read_port_commandtable(uint32_t abar, int portNb, int commandNb, struct ahci_port_commandtable* data)
+{
+    return 0;
+}
+
+int driver_ahci_get_ports_enabled(uint32_t abar)
+{
+    struct ahci_generic_host_regs target_hba;
+    driver_ahci_read_GHC_regs(abar, &target_hba);
+    
+    int np = AHCI_CAP_NP(target_hba.host_capabilities);
+    
+    return np + 1; // Add 1 because its 0 based count. 0 equals 1 port available
+}
+
+int driver_ahci_get_disk_ports(uint32_t abar, int* ports, size_t* amount)
+{
+    // Get the GHC regs.
+    struct ahci_generic_host_regs target_hba;
+    driver_ahci_read_GHC_regs(abar, &target_hba);
+    
+    // Each bit set in the PI byte represent a particular port being open.
+    // Need to go through each bit and check its its one then add it in the list
+    uint32_t portsImplemented = target_hba.ports_implemented;
+
+    // Next position in the 'ports' parameter to add a found port.
+    size_t portsIndex = 0;
+    
+    // Set the current 
+    for(int i = 0; i < 32; i++)
+    {
+        uint32_t res = 0x1 << i;
+        
+        if(FLAG(portsImplemented, res))
+        {
+            ports[portsIndex] = i;
+            portsIndex++;
+        }
+    }
+    
+    *amount = portsIndex;
+}
--- ahci_driver.h
+++ ahci_driver.h
@@ -1,6 +1,7 @@
 #ifndef AHCI_DRIVER_H
 #define AHCI_DRIVER_H
 
+#include <stddef.h>
 #include <stdint.h>
 
 // Generic Host Control registers
@@ -18,32 +19,32 @@
 #define AHCI_BOHC       0x28 // BIOS/OS Handoff Control and Status
 
 // AHCI_CAP Bytes
-#define AHCI_CAP_S64A   (x) (x & 1<<31) // #31 Supports 64-bit Addressing
-#define AHCI_CAP_SNCQ   (x) (x & 1<<30) // #30 Supports Native Command Queuing
-#define AHCI_CAP_SSNTF  (x) (x & 1<<29) // #29 Supports SNotification Register
-#define AHCI_CAP_SMPS   (x) (x & 1<<28) // #28 Supports Mechanical Presence Switch
-#define AHCI_CAP_SSS    (x) (x & 1<<27) // #27 Supports Staggered Spin-up
-#define AHCI_CAP_SALP   (x) (x & 1<<26) // #26 Supports Aggressive Link Power Management
-#define AHCI_CAP_SAL    (x) (x & 1<<25) // #25 Supports Activity LED
-#define AHCI_CAP_SCLO   (x) (x & 1<<24) // #24 Supports Command List Override
-#define AHCI_CAP_ISS    (x) (x & 0xF00000>>20) // #23:20 Interface Speed Support
-#define AHCI_CAP_SAM    (x) (x & 1<<18) // #18 Supports AHCI mode only
-#define AHCI_CAP_SPM    (x) (x & 1<<17) // #17 Supports Port Multiplier
-#define AHCI_CAP_FBSS   (x) (x & 1<<16) // #16 FIS-based Switching Supported
-#define AHCI_CAP_PMD    (x) (x & 1<<15) // #15 PIO Multiple DRQ Block
-#define AHCI_CAP_SSC    (x) (x & 1<<14) // #14 Slumber State Capable
-#define AHCI_CAP_PSC    (x) (x & 1<<13) // #13 Partial State Capable
-#define AHCI_CAP_NCS    (x) (x & 1<<12) // #12:08 Number of Command Slots
-#define AHCI_CAP_CCCS   (x) (x & 1<<7)  // #07 Command Completion Coalescing Supported
-#define AHCI_CAP_EMS    (x) (x & 1<<6)  // #06 Enclosure Management Supported
-#define AHCI_CAP_SXS    (x) (x & 1<<5)  // #05 Supports External SATA
-#define AHCI_CAP_NP     (x) (x & 0x1F)  // #04:00 Number of Ports
+#define AHCI_CAP_S64A(x)    (x & 1<<31) // #31 Supports 64-bit Addressing
+#define AHCI_CAP_SNCQ(x)    (x & 1<<30) // #30 Supports Native Command Queuing
+#define AHCI_CAP_SSNTF(x)   (x & 1<<29) // #29 Supports SNotification Register
+#define AHCI_CAP_SMPS(x)    (x & 1<<28) // #28 Supports Mechanical Presence Switch
+#define AHCI_CAP_SSS(x)     (x & 1<<27) // #27 Supports Staggered Spin-up
+#define AHCI_CAP_SALP(x)    (x & 1<<26) // #26 Supports Aggressive Link Power Management
+#define AHCI_CAP_SAL(x)     (x & 1<<25) // #25 Supports Activity LED
+#define AHCI_CAP_SCLO(x)    (x & 1<<24) // #24 Supports Command List Override
+#define AHCI_CAP_ISS(x)     (x & 0xF00000>>20) // #23:20 Interface Speed Support
+#define AHCI_CAP_SAM(x)     (x & 1<<18) // #18 Supports AHCI mode only
+#define AHCI_CAP_SPM(x)     (x & 1<<17) // #17 Supports Port Multiplier
+#define AHCI_CAP_FBSS(x)    (x & 1<<16) // #16 FIS-based Switching Supported
+#define AHCI_CAP_PMD(x)     (x & 1<<15) // #15 PIO Multiple DRQ Block
+#define AHCI_CAP_SSC(x)     (x & 1<<14) // #14 Slumber State Capable
+#define AHCI_CAP_PSC(x)     (x & 1<<13) // #13 Partial State Capable
+#define AHCI_CAP_NCS(x)     (x & 1<<12) // #12:08 Number of Command Slots
+#define AHCI_CAP_CCCS(x)    (x & 1<<7)  // #07 Command Completion Coalescing Supported
+#define AHCI_CAP_EMS(x)     (x & 1<<6)  // #06 Enclosure Management Supported
+#define AHCI_CAP_SXS(x)     (x & 1<<5)  // #05 Supports External SATA
+#define AHCI_CAP_NP(x)      (x & 0x1F)  // #04:00 Number of Ports
 
 // AHCI GHC Bytes
-#define AHCI_GHC_AE     (x) (x & 1<<31) // #31 AHCI Enable
-#define AHCI_GHC_MRSM   (x) (x & 1<<2)  // #02 MSI Revert to Single Message
-#define AHCI_GHC_IE     (x) (x & 1<<1)  // #01 Interrupt Enable
-#define AHCI_GHC_HR     (x) (x & 1)  // #00 HBA Reset
+#define AHCI_GHC_AE(x)      (x & 1<<31) // #31 AHCI Enable
+#define AHCI_GHC_MRSM(x)    (x & 1<<2)  // #02 MSI Revert to Single Message
+#define AHCI_GHC_IE(x)      (x & 1<<1)  // #01 Interrupt Enable
+#define AHCI_GHC_HR(x)      (x & 1)  // #00 HBA Reset
 
 struct ahci_generic_host_regs
 {
@@ -74,7 +75,7 @@ struct ahci_port_regs
     uint32_t interrupt_status;
     uint32_t interrupt_enable;
     uint32_t command_and_status;
-    uint32_t reserved;
+    uint32_t reserved1;
     uint32_t task_file_data;
     uint32_t signature;
     uint32_t serial_ata_status;
@@ -85,12 +86,70 @@ struct ahci_port_regs
     uint32_t serial_ata_notification;
     uint32_t fis_based_switching_control;
     uint32_t device_sleep;
-    uint32_t reserved;
+    uint32_t reserved2;
     uint32_t vendor_specific;
 };
 
-int driver_ahci_read_CAP_regs(struct ahci_hba_regs* regs);
+struct ahci_port_command_header
+{
+    uint16_t prdtl;
+    uint16_t prflags;
+    uint32_t prdByteCount;
+    uint32_t commandtableBaseAddr;
+    uint32_t commandtableBaseAddrUpper;
+    uint32_t reserved1;
+    uint32_t reserved2;
+    uint32_t reserved3;
+    uint32_t reserved4;
+};
 
-int driver_ahci_read_GHC_regs();
+struct ahci_port_commandlist
+{
+    struct ahci_port_command_header entries[32];
+};
+
+struct ahci_port_fis
+{
+    void* data;
+};
+
+struct ahci_port_commandtable
+{
+    void* data;
+};
+
+/**
+ * AHCI Memory overview
+ * 
+ * The PCI bus device has the BAR5(ABAR) and it stores the address in memory
+ * of the HBA.
+ * 
+ * The HBA has generic registers and a set of register for each port.
+ * 
+ * Each port has the address to a Command List and Received FIS struct.
+ * 
+ * The Command list struct contains a list of command tables
+ * 
+ */
+
+// Get the ABAR of the default disk.
+uint32_t driver_ahci_get_default_disk();
+
+/* Register reading */
+int driver_ahci_read_GHC_regs(uint32_t abar, struct ahci_generic_host_regs* regs);
+int driver_ahci_read_port_regs(uint32_t abar, int portNb, struct ahci_port_regs* regs);
+int driver_ahci_read_port_commandlist(uint32_t abar, int portNb, struct ahci_port_commandlist* data);
+int driver_ahci_read_port_commandtable(uint32_t abar, int portNb, int commandNb, struct ahci_port_commandtable* data);
+
+// Get the amount of ports supported by the machine not all of them may be 
+// implemented. Not such a useful function because we can just call 
+// driver_ahci_get_disk_ports and get the current amount of ports usable.
+int driver_ahci_get_ports_enabled();
+
+// Get the ports where a disk is present.
+// 'ports' must be initialized as an array of 32 ints all filled with 0.
+// 'amount' is the number of disks found, the first 'amount' elements of the 'ports'
+// array will be filled with the occupied port of that disk.
+int driver_ahci_get_disk_ports(uint32_t abar, int* ports, size_t* amount);
 
 #endif
